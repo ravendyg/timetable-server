d73ff730cdd777d5a78a0b48bc63eec2a075e00d
Yaroslav <jaroslav.dashkevich@gmail.com>
2017-03-09 23:18:31 +0700

    Add ability to use aliases

--- routes/api2.js
+++ routes/api2.js
@@ -7,6 +7,9 @@ const assert = require('assert');
 const crawler = require('../lib/services/crawler');
 
 const logDb = require('../lib/storage/log');
+const mapsDb = require('../lib/storage/maps');
+const namesDb = require('../lib/storage/names');
+const aliasesDb = require('../lib/storage/aliases');
 
 
 router.get(
@@ -63,6 +66,32 @@ router.get(
     catch (err)
     {
       res.status(404).send();
+    }
+  }
+);
+
+router.put(
+  '/alias',
+  function putAlias(req, res)
+  {
+    if (!(req.body.type === 'events' && crawler.getEventList().indexOf(req.body.id.replace(/\./g, '')) !== -1 ||
+        ['teachers', 'places'].indexOf(req.body.type) !== -1 && crawler.syncData(req.body.type, 0, req.body.id.replace(/\./g, '')).length > 0) ||
+        !req.body.alias)
+    {
+      res.statusCode(400);
+      res.end();
+    }
+    else
+    {
+      aliasesDb.putAlias({
+        type:  req.body.type,
+        key:   req.body.id.replace(/\./g, ''),
+        value: req.body.alias,
+      });
+      crawler.reset();
+      mapsDb.drop();
+      namesDb.drop();
+      res.status(204).send();
     }
   }
 );
